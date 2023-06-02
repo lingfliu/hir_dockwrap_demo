b1a72490ce6dd286bd25dfafac91e122250a9d8f
lingfliu <lingfeng.liu@163.com>
2023-06-02 13:33:48 +0800

    updated

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ Config = {
     'mqtt_username': 'mx01',
     'mqtt_password': 'mx123456',
     'mqtt_host': '192.168.0.209',
-    'mqtt_port': '1883',
+    'mqtt_port': 1883,
     'file_server': 'http://192.168.0.209:7394',
     'file_demo': 'a0382a321bf9447ea99c6cb7a7f836da.jpg'
 }
